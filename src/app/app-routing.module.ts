378d5217511c5d0572be83fa71f69986b19a59f7
abbasi-naim <abbasi.naim@gmail.com>
2020-11-21 22:23:24 +0330

    Added charaktere and planeten path to route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharakterePageComponent } from './components/charaktere-page/charaktere-page.component';
 import { FilmePageComponent } from './components/filme-page/filme-page.component';
+import { PlanetenPageComponent } from './components/planeten-page/planeten-page.component';
 import { StarwarsPageComponent } from './components/starwars-page/starwars-page.component';
 const routes: Routes = [
-  {path : 'starwars',component : StarwarsPageComponent},
-  {path : 'film',component : FilmePageComponent},
+  {path : 'starwars', component : StarwarsPageComponent},
+  {path : 'film', component : FilmePageComponent},
+  {path : 'character', component : CharakterePageComponent},
+  {path : 'planet', component : PlanetenPageComponent},
 ];
 
 @NgModule({
