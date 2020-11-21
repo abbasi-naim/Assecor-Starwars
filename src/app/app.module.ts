afae02d98406f2db47981facc4b2835b5cadfe9e
abbasi-naim <abbasi.naim@gmail.com>
2020-11-21 21:08:25 +0330

    added Http and fontAwesome module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navBar/navbar/navbar.component';
 import { StarwarsPageComponent } from './components/starwars-page/starwars-page.component';
 import { FilmePageComponent } from './components/filme-page/filme-page.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { FilmePageComponent } from './components/filme-page/filme-page.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
