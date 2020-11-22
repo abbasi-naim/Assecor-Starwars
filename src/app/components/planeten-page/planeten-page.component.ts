357940c55cfca8b1679acfd13d7b84ed560f3061
abbasi-naim <abbasi.naim@gmail.com>
2020-11-22 18:07:50 +0330

    Added class on planeten-page-component to retrieve data from Proper API Url

--- src/app/components/planeten-page/planeten-page.component.ts
+++ src/app/components/planeten-page/planeten-page.component.ts
@@ -1,15 +1,39 @@
+import { SwapiDataService } from './../../swapi-data.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-planeten-page',
   templateUrl: './planeten-page.component.html',
-  styleUrls: ['./planeten-page.component.css']
+  styleUrls: ['./planeten-page.component.css'],
 })
 export class PlanetenPageComponent implements OnInit {
+  planets = [];
+  showDetail = false;
+  selectedPlanet = { filmDetails: [] };
+  constructor(private dataService: SwapiDataService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.dataService.getAllPlanets().subscribe((data: any) => {
+      console.log(data);
+      this.planets = data.results;
+    });
   }
-
+  //Add details for each planet
+  onShowDetail(t) {
+    let filmDetails = [];
+    this.selectedPlanet = t;
+    this.showDetail = true;
+    t.films.forEach((film) => {
+      this.dataService.getByUrl(film).subscribe((data: any) => {
+        data.url = film;
+        filmDetails.push(data);
+      });
+    });
+    this.selectedPlanet.filmDetails = filmDetails;
+    console.log(this.selectedPlanet.filmDetails);
+  }
+  //Hide details when button clicked
+  onHideDetail() {
+    this.showDetail = false;
+  }
 }
