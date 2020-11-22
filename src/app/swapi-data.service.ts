f4be380dffffb007fd47e44aeec275b336061d54
abbasi-naim <abbasi.naim@gmail.com>
2020-11-22 17:49:19 +0330

    Added Star Wars API URL

--- src/app/swapi-data.service.ts
+++ src/app/swapi-data.service.ts
@@ -2,24 +2,38 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
+
+//Add  StarWars API URL
 export class SwapiDataService {
+  private FILMS_REST_API_URL = 'https://swapi.dev/api/films/';
+  private CHARACTER_REST_API_URL = 'https://swapi.dev/api/people/';
+  private PLANET_REST_API_URL = 'https://swapi.dev/api/planets/';
 
-  private FILMS_REST_API_URL = "https://swapi.dev/api/films/";
-  private CHARACTER_REST_API_URL = "https://swapi.dev/api/people/";
+  constructor(private httpClient: HttpClient) {}
 
-  constructor(private httpClient: HttpClient) { }
-  public getAllFilms(){
+  //Add Function to get all films Data
+  public getAllFilms() {
     return this.httpClient.get(this.FILMS_REST_API_URL);
   }
-  public getAllCharacters(){
+
+  //Add Function to get all Character Data
+  public getAllCharacters() {
     return this.httpClient.get(this.CHARACTER_REST_API_URL);
   }
-  public getAllCharactersByID(id){
+
+  //Add Function to get all Character Data by their IDs
+  public getAllCharactersByID(id) {
     return this.httpClient.get(this.CHARACTER_REST_API_URL + id);
   }
-  public getByUrl(url){
+
+  //Add Function to get all Planet Data
+  public getAllPlanets() {
+    return this.httpClient.get(this.PLANET_REST_API_URL);
+  }
+
+  public getByUrl(url) {
     return this.httpClient.get(url);
   }
 }
