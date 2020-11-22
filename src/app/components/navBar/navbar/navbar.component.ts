cefa52753614ffe96a2c7d302b8d2c2c6dee57f9
abbasi-naim <abbasi.naim@gmail.com>
2020-11-22 22:45:15 +0330

    Added Condition to routerLink to make the active link unclickable

--- src/app/components/navBar/navbar/navbar.component.ts
+++ src/app/components/navBar/navbar/navbar.component.ts
@@ -4,14 +4,18 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
   faSearch = faSearch;
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  //To control navbar links whether they are clicked or not
+  starWarsLinkEnabled: boolean = true;
+  filmLinkEnabled: boolean = true;
+  characterLinkEnabled: boolean = true;
+  planetLinkEnabled: boolean = true;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
