815553f632a142c247e8151084142e03981e6ff5
abbasi-naim <abbasi.naim@gmail.com>
2020-11-22 17:13:08 +0330

    Added Character API Data for Charaktere-page

--- src/app/components/charaktere-page/charaktere-page.component.ts
+++ src/app/components/charaktere-page/charaktere-page.component.ts
@@ -1,15 +1,38 @@
+import { SwapiDataService } from './../../swapi-data.service';
 import { Component, OnInit } from '@angular/core';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-charaktere-page',
   templateUrl: './charaktere-page.component.html',
-  styleUrls: ['./charaktere-page.component.css']
+  styleUrls: ['./charaktere-page.component.css'],
 })
 export class CharakterePageComponent implements OnInit {
+  characters = [];
+  showDetail = false;
+  selectedCharacter = { filmDetails: [] };
+  constructor(private dataService: SwapiDataService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.dataService.getAllCharacters().subscribe((data: any) => {
+      console.log(data);
+      this.characters = data.results;
+    });
   }
-
+  onShowDetail(t) {
+    let filmDetails = [];
+    this.selectedCharacter = t;
+    this.showDetail = true;
+    t.films.forEach((film) => {
+      this.dataService.getByUrl(film).subscribe((data: any) => {
+        data.url = film;
+        filmDetails.push(data);
+      });
+    });
+    this.selectedCharacter.filmDetails = filmDetails;
+    console.log(this.selectedCharacter.filmDetails);
+  }
+  onHideDetail() {
+    this.showDetail = false;
+  }
 }
