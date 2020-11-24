c5e67c4fae3d20be6c9061f5e21c37924c93dee2
abbasi-naim <abbasi.naim@gmail.com>
2020-11-24 23:31:46 +0330

    Added Global animation to starwars page

--- src/app/components/starwars-page/starwars-page.component.ts
+++ src/app/components/starwars-page/starwars-page.component.ts
@@ -1,31 +1,11 @@
+import { dissolve } from './../../animations';
 import { Component, OnInit } from '@angular/core';
-import {
-  trigger,
-  animate,
-  transition,
-  style,
-  useAnimation,
-} from '@angular/animations';
 
 @Component({
   selector: 'app-starwars-page',
   templateUrl: './starwars-page.component.html',
   styleUrls: ['./starwars-page.component.css'],
-  animations: [
-    trigger('Tap', [
-      // The '* => *' will trigger the animation to change between any two states
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('0.3s'),
-        style({ opacity: 1 }),
-      ]),
-      transition(':leave', [
-        style({ opacity: 1 }),
-        animate('0.3s ease-out'),
-        style({ opacity: 0 }),
-      ]),
-    ]),
-  ],
+  animations: [dissolve],
 })
 export class StarwarsPageComponent implements OnInit {
   constructor() {}
