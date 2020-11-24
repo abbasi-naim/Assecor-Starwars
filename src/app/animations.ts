d17098af5a32a60ff320a9ea6b5dd71287c22e5c
abbasi-naim <abbasi.naim@gmail.com>
2020-11-24 23:07:55 +0330

    Optimize animation for globaly use in every page

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,6 +1,21 @@
-import { animation, animate, style } from '@angular/animations';
+import {
+  trigger,
+  animate,
+  transition,
+  style,
+  useAnimation,
+} from '@angular/animations';
 
-export const transAnimation = animation([
-  style({ opacityIn: '{{opacity}}' }),
-  animate('{{ time }}', style({ opacityOut: '{{opacity}}' })),
+//added animation for fade in in each page
+export let dissolve = trigger('Tap', [
+  transition(':enter', [
+    style({ opacity: 0 }),
+    animate('0.3s'),
+    style({ opacity: 1 }),
+  ]),
+  transition(':leave', [
+    style({ opacity: 1 }),
+    animate('0.3s ease-out'),
+    style({ opacity: 0 }),
+  ]),
 ]);
