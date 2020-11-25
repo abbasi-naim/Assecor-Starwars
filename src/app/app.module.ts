e90d614356824ffa04041e58da6fe7fc42f6e4d5
abbasi-naim <abbasi.naim@gmail.com>
2020-11-26 02:06:16 +0330

    Added Grid list module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { PlanetenPageComponent } from './components/planeten-page/planeten-page.
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/footer/footer.component';
 import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -30,6 +31,7 @@ import { MatButtonModule } from '@angular/material/button';
     FontAwesomeModule,
     BrowserAnimationsModule,
     MatButtonModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
